d3cdc29b18a2fdaf86ef6d800c6219a1bdd9d4c0
Hernany Dias <mdkdemok2@gmail.com>
2018-08-02 05:03:49 -0300

    Deletei o model contatos pq a proposta do formulario de contato nao era armazenar os dados no serv e sim enviar por email.

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -5,17 +5,25 @@
             $dados = array();
 
             if(isset($_POST['nome']) && !empty($_POST['nome'])){
-            	if(isset($_POST['email']) && !empty($_POST['email'])){
-            		$nome = addslashes($_POST['nome']);
-            		$email = addslashes($_POST['email']);
-            		$msg = addslashes($_POST['mensagem']);
-
-            		$s = new Contatos();
-            		$s->addContato($nome, $email, $msg);
-               		echo '<h4 style="text-align:center;">Mensagem enviados com sucesso, Obrigado !</h4>';
-   
-            	}
-            } 
+            
+                $nome = addslashes($_POST['nome']);
+                $email = addslashes($_POST['email']);
+                $msg = addslashes($_POST['mensagem']);
+                
+                $para = "suporte@site.com.br";
+                $assunto = "Dúvida do site";
+                $mensagem = "Nome:".$nome."<br/>E-mail: ".$email."<br/>Mensagem: ".$msg;
+                
+                $cabecalho = 'From: suporte@site.com.br'. "\r\n".'Reply-To: '.$email. "\r\n".
+                'X-Mailer: PHP/'.phpversion();
+                mail($para, $assunto, $mensagem, $cabecalho);
+       
+                echo '<h4 style="text-align:center;">Mensagem enviados com sucesso, Obrigado !</h4>';
+            }
+    
+               		
+            	
+             
 
             
             $this->loadView('home', $dados);
